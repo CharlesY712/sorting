ab5751e0a222fd71537f2032626c59f936e9d0a3
Charles Yach <CharlesY712@gmail.com>
2018-02-08 08:49:19 -0700

    Updating tests with checkSort function

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,5 +1,37 @@
-import { expect } from 'chai';
-import index from '../lib/index'
+// import { expect } from 'chai';
+// import index from '../lib/index.js'
+// import {bubbleSort} from "@charlesy712/charles-module"
 
-describe('Example Test File', () => {
-})
+// describe('bubbleSort', () => {
+//   it('should sort a small array of numbers', () => {
+//     let array = [2,8,4,3,6,1,5,7];
+//     let sortedArray = bubbleSort(array);
+//     expect(array).to.deep.equal([1,2,3,4,5,6,7,8]);
+//   })
+
+//   it('should sort letters alphabetically', () => {
+//     let array = ['c', 'h', 'a', 'e', 'b', 'f', 'd', 'g'];
+//     let sortedArray = bubbleSort(array);
+//     expect(array).to.deep.equal(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
+//   })
+
+//   it('should sort positive and negative numbers', () => {
+//     let array = [-2,3,6,-4,1,-8,5,7];
+//     let sortedArray = bubbleSort(array);
+//     expect(array).to.deep.equal([-8,-4,-2,1,3,5,6,7]);
+//   })
+
+//   it('should sort a large array of numbers', () => {
+//     let array = [];
+//     let sortedArray = bubbleSort(array);
+
+//     let min = -100
+//     let max = 10000000
+
+//     for (let i = 0; i < Math.abs(min) + max; i++) {
+//       let number = Math.floor(Math.random() * (max - min)) + min;
+//       array.push(number);
+//     }
+//     expect(array).to.deep.equal(sortedArray);
+//   })
+// });
---
+++ tests/mergeSort-test.js
@@ -0,0 +1,54 @@
+import { expect } from 'chai';
+import mergeSort from '../lib/mergeSort.js';
+
+function sortCheck (array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      return false;
+    } else {
+      return array;
+    }
+  }
+}
+
+describe('mergeSort', () => {
+  it('should sort a small array of numbers', () => {
+    let array = [2, 8, 4, 3, 6, 1, 5, 7];
+    let sortedArray = mergeSort(array);
+
+    expect(sortedArray).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it('should sort letters alphabetically', () => {
+    let array = ['c', 'h', 'a', 'e', 'b', 'f', 'd', 'g'];
+    let sortedArray = mergeSort(array);
+
+    expect(sortedArray).to.deep.equal(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
+  });
+
+  it('should sort positive and negative numbers', () => {
+    let array = [-2, 3, 6, -4, 1, -8, 5, 7];
+    let sortedArray = mergeSort(array);
+
+    expect(sortedArray).to.deep.equal([-8, -4, -2, 1, 3, 5, 6, 7]);
+  });
+
+  it('should sort a large array of numbers', () => {
+    let array = [];
+    let min = -100;
+    let max = 27000;
+    // let max = 1,500,000
+
+    for (let i = 0; i < Math.abs(min) + max; i++) {
+      let number = Math.floor(Math.random() * (max - min)) + min;
+
+      array.push(number);
+    }
+
+    let sortedArray = mergeSort(array);
+
+    expect(sortedArray).to.deep.equal(sortCheck(sortedArray));
+  });
+});
+
+
